2def1e1101fed7c5d11594f2a1b47772f71fb95f
Petya Mindalova <petmindalova@gmail.com>
2026-04-14 14:43:25 +0300

    3 new negative registration tests

--- tests/accountRegistration.spec.ts
+++ tests/accountRegistration.spec.ts
@@ -12,7 +12,7 @@ test.describe('Registration Page Tests', () => {
     await page.goto(testConfig.appUrl);
   });
 
-  test('User can navigate to Registration page @smoke', async ({ page }) => {
+  test('1.User can navigate to Registration page @smoke', async ({ page }) => {
     const homePage = new HomePage(page);
     const registrationPage = new RegisterPage(page);
 
@@ -21,7 +21,7 @@ test.describe('Registration Page Tests', () => {
     await registrationPage.isRegisterPageLoaded();
   });
 
-    test('User can successfully register with valid data @regression', async ({ page }) => {
+    test('2.User can successfully register with valid data @regression', async ({ page }) => {
       const homePage = new HomePage(page);
       const registrationPage = new RegisterPage(page);
 
@@ -33,6 +33,55 @@ test.describe('Registration Page Tests', () => {
         RandomDataUtil.getRandomPassword()
       );
         await expect(page).toHaveURL(/route=account\/success/);
+    });
+
+    test('3.User cannot register with an already existing email @regression', async ({ page }) => {
+      const homePage = new HomePage(page);
+
+      await homePage.goToRegisterPage();
+      await page.getByRole('textbox', { name: 'First Name' }).fill(RandomDataUtil.getFirstName());
+      await page.getByRole('textbox', { name: 'Last Name' }).fill(RandomDataUtil.getlastName());
+      await page.getByRole('textbox', { name: 'E-Mail' }).fill(testConfig.email);
+      await page.getByRole('textbox', { name: 'Password' }).fill(RandomDataUtil.getRandomPassword());
+      await page.locator('[name="agree"]').check();
+      await page.getByRole('button', { name: 'Continue' }).click();
+
+      await expect(page.getByText(/already registered/i)).toBeVisible();
+      await expect(page).toHaveURL(/route=account\/register/);
+    });
+
+    test('4.User cannot register with invalid email format @regression', async ({ page }) => {
+      const homePage = new HomePage(page);
+
+      await homePage.goToRegisterPage();
+      await page.getByRole('textbox', { name: 'First Name' }).fill(RandomDataUtil.getFirstName());
+      await page.getByRole('textbox', { name: 'Last Name' }).fill(RandomDataUtil.getlastName());
+      await page.getByRole('textbox', { name: 'E-Mail' }).fill('invalid-email-format');
+      await page.getByRole('textbox', { name: 'Password' }).fill(RandomDataUtil.getRandomPassword());
+      await page.locator('[name="agree"]').check();
+      await page.getByRole('button', { name: 'Continue' }).click();
+
+      await expect(page).toHaveURL(/route=account\/register/);
+      const emailInput = page.locator('#input-email');
+
+      const validationMessage = await emailInput.evaluate((el: HTMLInputElement) => el.validationMessage);
+
+      expect(validationMessage).toContain("Please include an '@'");
+      
+    });
+
+    test('5.User cannot register with empty first name @regression', async ({ page }) => {
+      const homePage = new HomePage(page);
+
+      await homePage.goToRegisterPage();
+      await page.getByRole('textbox', { name: 'Last Name' }).fill(RandomDataUtil.getlastName());
+      await page.getByRole('textbox', { name: 'E-Mail' }).fill(RandomDataUtil.getEmail());
+      await page.getByRole('textbox', { name: 'Password' }).fill(RandomDataUtil.getRandomPassword());
+      await page.locator('[name="agree"]').check();
+      await page.getByRole('button', { name: 'Continue' }).click();
+
+      await expect(page).toHaveURL(/route=account\/register/);
+      await expect(page.locator('#error-firstname')).toHaveText('First Name must be between 1 and 32 characters!');
     });
 
 
