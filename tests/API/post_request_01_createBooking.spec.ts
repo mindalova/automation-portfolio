98977001748476082fcc7e45b1706d65ab878cc6
Petya Mindalova <petmindalova@gmail.com>
2026-07-04 14:13:48 +0300

    folders changed

---
+++ tests/API/post_request_01_createBooking.spec.ts
@@ -0,0 +1,53 @@
+/*
+Create a new booking
+Validate the response
+Validate the response body
+Validate the response headers
+Validate the response status code
+Validate the response time
+Validate the response body
+Validate the response headers
+Validate the response status code
+Validate the response time
+*/
+
+import { test, expect } from '@playwright/test';
+
+test('Create a new booking Post Request', async ({ request }) => {
+  const requestBody = {
+    firstname: 'Bon3',
+    lastname: 'Jovi3',
+    totalprice: 111,
+    depositpaid: true,
+    bookingdates: {
+      checkin: '2026-01-01',
+      checkout: '2027-01-01',
+    },
+    additionalneeds: 'Breakfast',
+  };
+
+  const response = await request.post('/booking', {
+    data: requestBody,
+  });
+
+  const responseBody = await response.json();
+
+  console.log(responseBody);
+
+  expect(response.status()).toBe(200);
+  expect(responseBody.bookingid).toBeDefined();
+  expect(responseBody).toHaveProperty('bookingid');
+  expect(responseBody.booking.firstname).toBe(requestBody.firstname);
+  expect(responseBody.booking.lastname).toBe(requestBody.lastname);
+  expect(responseBody.booking.totalprice).toBe(requestBody.totalprice);
+  expect(responseBody.booking.depositpaid).toBe(requestBody.depositpaid);
+  expect(responseBody.booking.bookingdates.checkin).toBe(
+    requestBody.bookingdates.checkin,
+  );
+  expect(responseBody.booking.bookingdates.checkout).toBe(
+    requestBody.bookingdates.checkout,
+  );
+  expect(responseBody.booking.additionalneeds).toBe(
+    requestBody.additionalneeds,
+  );
+});
---
+++ tests/API/post_request_03_createBooking.spec.ts
@@ -0,0 +1,50 @@
+/*
+Create a new booking
+Request body is random data from faker library
+Request type is POST
+*/
+
+import { test, expect } from '@playwright/test';
+import { RandomDataUtil } from '../../utils/randomDataGenerator';
+
+test('Create a new booking Post Request using faker data', async ({
+  request,
+}) => {
+  const requestBody = {
+    firstname: RandomDataUtil.getFirstName(),
+    lastname: RandomDataUtil.getlastName(),
+    totalprice: RandomDataUtil.getRandomPrice(100, 1000),
+    depositpaid: RandomDataUtil.getRandomBoolean(),
+    bookingdates: {
+      checkin: RandomDataUtil.getRandomDate(),
+      checkout: RandomDataUtil.getRandomDate(),
+    },
+    additionalneeds: RandomDataUtil.getRandomString(10),
+  };
+  console.log(requestBody);
+
+  const response = await request.post('/booking', {
+    data: requestBody,
+  });
+
+  const responseBody = await response.json();
+
+  console.log(responseBody);
+
+  expect(response.status()).toBe(200);
+  expect(responseBody.bookingid).toBeDefined();
+  expect(responseBody).toHaveProperty('bookingid');
+  expect(responseBody.booking.firstname).toBe(requestBody.firstname);
+  expect(responseBody.booking.lastname).toBe(requestBody.lastname);
+  expect(responseBody.booking.totalprice).toBe(requestBody.totalprice);
+  expect(responseBody.booking.depositpaid).toBe(requestBody.depositpaid);
+  expect(responseBody.booking.bookingdates.checkin).toBe(
+    requestBody.bookingdates.checkin,
+  );
+  expect(responseBody.booking.bookingdates.checkout).toBe(
+    requestBody.bookingdates.checkout,
+  );
+  expect(responseBody.booking.additionalneeds).toBe(
+    requestBody.additionalneeds,
+  );
+});
